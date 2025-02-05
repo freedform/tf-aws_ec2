7c00fa5ee462e9e8d78f7cdda2cbdcd20b714d50
freedform <62791746+freedform@users.noreply.github.com>
2025-02-05 22:25:57 +0500

    Count optimization

--- main.tf
+++ main.tf
@@ -1,9 +1,11 @@
 locals {
-  user_data = templatefile("${path.module}/user_data.sh", {
-    hostname                    = var.hostname
-    user_data_script            = var.user_data
-    check_internet_connectivity = var.check_internet_connectivity
-  })
+  user_data = [
+    for item in range(1, var.instance_count + 1) : templatefile("${path.module}/user_data.sh", {
+      hostname                    = var.instance_count > 1 && var.hostname != "" ? "${var.hostname}-${item}" : var.hostname
+      user_data_script            = var.user_data
+      check_internet_connectivity = var.check_internet_connectivity
+    })
+  ]
   user_data_check = templatefile("${path.module}/user_data_check.sh", {
     timeout = var.user_data_check_timeout
   })
@@ -18,9 +20,11 @@ resource "aws_instance" "ec2_instance" {
   key_name                    = var.key_name
   vpc_security_group_ids      = var.security_groups
   subnet_id                   = var.subnets[count.index % length(var.subnets)]
-  user_data                   = local.user_data
+  user_data_base64            = base64encode(local.user_data[count.index])
   user_data_replace_on_change = var.user_data_replace_on_change
-  tags                        = merge({}, var.tags)
+  tags                        = merge({
+    Name = var.instance_count > 1 && var.hostname != "" ? "${var.hostname}-${count.index + 1}" : var.hostname
+  }, var.tags)
 }
 
 resource "null_resource" "check_user_data" {
